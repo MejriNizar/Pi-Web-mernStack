83d3cab21155c00dbae522e1128ebaa6c9715f69
nizar04 <33844522+nizar04@users.noreply.github.com>
2020-03-03 18:16:53 +0100

    mail done for sure

--- config/mailer.js
+++ config/mailer.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MAILGUN_USER:'postmaster@sandboxa72b1b9cd9da487aaefd4fb2460808e9.mailgun.org',
-    MAILGUN_PASS:'824d167da239d21ef586c2e710e03240-c322068c-f601c2f6'
+    MAILGUN_USER:'nizar.mejri@esprit.tn',
+    MAILGUN_PASS:'28526044'
 }
--- misc/mailer.js
+++ misc/mailer.js
@@ -2,7 +2,7 @@ const nodemailer =require('nodemailer');
 const config = require('../config/mailer');
 
 const transport = nodemailer.createTransport({
-   service: 'MailGun',
+   service: 'gmail',
    auth: {
        user: config.MAILGUN_USER,
        pass: config.MAILGUN_PASS
