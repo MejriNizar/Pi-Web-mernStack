762d461b545108e3ea55636adc52c27e31b7ac2f
nizar04 <33844522+nizar04@users.noreply.github.com>
2020-03-25 19:00:11 +0100

    Update DashboardAction.js

--- client/src/components/dashboard/DashboardAction.js
+++ client/src/components/dashboard/DashboardAction.js
@@ -6,25 +6,26 @@ import PropTypes from 'prop-types'
 const DashboardAction = ({auth:{user}}) => {
     return (
         <div className="dash-buttons">
-          {user.role === 'etudiant' ?( <Fragment><Link to="/edit-profile" className="btn btn-light"
+          {user.role === 'Student' ?( <Fragment><Link to="/edit-profile" className="btn btn-light"
           ><i className="fas fa-user-circle text-primary"></i> Edit Profile</Link>
         <Link to="/add-experience" className="btn btn-light"
           ><i className="fab fa-black-tie text-primary"></i> Add Experience</Link>
         <Link to="/add-education" className="btn btn-light"
           ><i className="fas fa-graduation-cap text-primary"></i> Add Education</Link>
-          <Link to="/all-project" className="btn btn-light"
-          ><i className="fas fa-graduation-cap text-primary"></i> List Project</Link>
-          <Link to="/all-events" className="btn btn-light"
-          ><i className="fa fa-th-list text-primary"></i> List events</Link>
-          <Link to="/add-event" className="btn btn-light"
-          ><i className="fa fa-plus-square text-primary"></i> add event</Link>
-          <Link to="/add-project" className="btn btn-light"
-          ><i className="fas fa-graduation-cap text-primary"></i> List Projects</Link></Fragment>):( <Fragment><Link to="/add-project" className="btn btn-light"
-          ><i className="fas fa-graduation-cap text-primary"></i> Add Project</Link>
+        
+      
           <Link to="/all-group" className="btn btn-light"
           ><i className="fas fa-graduation-cap text-primary"></i> List Groups</Link>
           <Link to="/add-group" className="btn btn-light"
-          ><i className="fas fa-graduation-cap text-primary"></i> Add Group</Link> </Fragment>) }
+          ><i className="fas fa-graduation-cap text-primary"></i> Add Group</Link>
+         
+          <Link to="/all-project" className="btn btn-light"
+          ><i className="fas fa-graduation-cap text-primary"></i> List Projects</Link></Fragment>):( <Fragment>  <Link to="/all-events" className="btn btn-light"
+          ><i className="fa fa-th-list text-primary"></i> List events</Link>
+          <Link to="/add-event" className="btn btn-light"
+          ><i className="fa fa-plus-square text-primary"></i> add event</Link>
+              <Link to="/add-project" className="btn btn-light"
+          ><i className="fas fa-graduation-cap text-primary"></i> Add Project</Link></Fragment>) }
         
          
       </div>
