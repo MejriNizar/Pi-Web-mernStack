a38bd44981708d5007fa36bc987565b5b7b23677
nizar04 <33844522+nizar04@users.noreply.github.com>
2020-04-06 18:58:46 +0100

    CV view incomplete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -299,28 +299,34 @@ router.delete(
 );
 
 
-router.post('/upload',auth,async(req,res)=>{
- try {
-  if(req.files == null) {
-    return res.status(400).json({ msg: 'No file uploaded'});
-  }
-  const file = req.files.file;
 
-  file.mv(`${__dirname}../../../client/public/uploads/${file.name}`, err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+router.post('/upload',auth,async(req,res)=>{
+  try {
+    if(req.files == null) {
+      return res.status(400).json({ msg: 'No file uploaded'});
     }
+    const file = req.files.file;
+  
+    file.mv(`${__dirname}../../../client/public/uploads/${file.name}`, err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+     
+      res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    });
     const profileFields={};
     profileFields.CV='/uploads/'+file.name;
 
-     Profile.findOneAndUpdate({user: req.user.id},{$set: profileFields});
-    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-  });
- } catch (err) {
-  console.error(err);
-  return res.status(500).send(err);
- }
+   await Profile.findOneAndUpdate({user: req.user.id},{$set: profileFields});
+    const profile =await Profile.findOne({user: req.user.id});
+    console.log(profile)
+  
+}
+   catch (err) {
+    console.error(err);
+    return res.status(500).send(err);
+  }
 })
 
 module.exports = router;
