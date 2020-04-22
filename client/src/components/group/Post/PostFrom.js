9556b953e585ebcc702a4112fdff60474912159c
Naderab <34838945+Naderab@users.noreply.github.com>
2020-04-22 22:01:13 +0100

    Commit

--- client/src/components/group/Post/PostFrom.js
+++ client/src/components/group/Post/PostFrom.js
@@ -23,7 +23,7 @@ const PostFrom = ({addPost}) => {
             onChange={e => setText(e.target.value)}
             required
           ></textarea>
-          <input type="submit" className="btn btn-dark my-1" value="Submit" />
+          <input type="submit" className="btn btn-primary my-1" value="Submit" />
         </form>
       </div>
     )
